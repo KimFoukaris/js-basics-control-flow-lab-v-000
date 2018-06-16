ad3f46825d43cb28a0c8e443a553c33f5f218aa0
Kim Foukaris <kimfoukaris@outlook.com>
2018-06-16 09:34:49 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 function scuberGreetingForFeet(rideLength) {
   let cost
-  if (rideLength<=400) {
+  if (rideLength <= 400) {
     cost = "This one is on me!";
+  } else if (rideLength > 2000 && rideLength <= 2500) {
+  cost = "I will gladly take your thirty bucks."
+  } else if (rideLength > 2500) {
+  cost = "No can do."
   }
   return cost
 }
